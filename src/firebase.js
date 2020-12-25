1ff3d835c96bda33087d981eff47ee961aa8fb68
Santos Bright <itzsantosbright15@gmail.com>
2020-12-25 06:38:43 +0100

    setup react

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtGYu6O5QCHYtv1AzlqUtdhExnR7QGbD8",
+    authDomain: "whatsapp-clone-19855.firebaseapp.com",
+    projectId: "whatsapp-clone-19855",
+    storageBucket: "whatsapp-clone-19855.appspot.com",
+    messagingSenderId: "870264507130",
+    appId: "1:870264507130:web:fe33ead72313e9076d1743",
+};
